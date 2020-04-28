dfbaf15cf2be87709a1f96bb39729aa892f439af
Siddharth Sheladiya <siddharthsheladiya@gmail.com>
2020-04-27 22:35:08 -0700

    home page

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pitcher.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pitcher_app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

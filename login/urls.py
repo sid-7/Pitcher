e52211496026b9df7c64f33b7f9bfaba88c57c8f
Siddharth Sheladiya <siddharthsheladiya@gmail.com>
2020-04-27 22:19:57 -0700

    testing firebase

--- login/urls.py
+++ login/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('verify' ,views.verify, name='verify'),
     path('pitcher',views.pitcher,name='pitcher'),
     path('contributor',views.contributor,name='contributor'),
-    path('investor',views.investor,name='investor')
+    path('investor',views.investor,name='investor'),
+    path('login_page_with_firebase', views.login_page_with_firebase, name='login_page_with_firebase'),
+    path('firebase_login', views.firebase_login, name='firebase_login')
 ]

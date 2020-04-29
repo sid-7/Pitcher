63ab291c1b5913560a3676d83f4736b4c4b98847
Siddharth Sheladiya <siddharthsheladiya@gmail.com>
2020-04-28 21:02:13 -0700

    media connectivity

--- pitcher/urls.py
+++ pitcher/urls.py
@@ -1,28 +1,10 @@
-"""pitcher URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path,include
-from django.conf.urls.static import static
-from django.conf import settings
+from . import views
 
 urlpatterns = [
-    path('', include('login.urls')),
-    path('admin/', admin.site.urls),
-    path('register/',include('register.urls'))
-]
+    path('dashboard/',views.dashboard, name='pitcher-dashboard'),
+    path('add_details/',views.add_details, name='pitcher-add_details'),
+    path('add_details_to_database/',views.add_details_to_database, name='pitcher-add_details_to_database')
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) #for media
+]
--- register/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('register',views.register, name='register'),
-    path('add_to_database',views.add_to_database, name='add_to_database')
-]

356ce826d81e6a46321972fbe6c3e0fbde6dbf2d
monilnisar4900 <monilnisar4900@gmail.com>
2020-04-29 01:22:02 -0700

    m

--- pitcher/urls.py
+++ pitcher/urls.py
@@ -4,7 +4,5 @@ from . import views
 
 urlpatterns = [
     path('dashboard/',views.dashboard, name='pitcher-dashboard'),
-    path('add_details/',views.add_details, name='pitcher-add_details'),
-    path('add_details_to_database/',views.add_details_to_database, name='pitcher-add_details_to_database')
-
+    path('new pitch/',views.new_pitch, name='pitcher-new_pitch'),
 ]
--- pitcher/views.py
+++ pitcher/views.py
@@ -5,24 +5,31 @@ from pitcher.models import AddDetails
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'pitcher/dashboard.html')
+    # pitches = all pitches    ########
+    # {'pitches': pitches}
+    if(True):
+        userid = 'n'
+        all_pitches = AddDetails.objects.filter(userid=userid)
+        print(all_pitches)
+        return render(request, 'pitcher/dashboard.html', {'pitches':all_pitches})
+    else:
+        return render(request, 'pitcher/dashboard.html')
 
-
-def add_details(request):
-    return render(request, 'pitcher/add_details.html')
-
-
-def add_details_to_database(request):
-    title = request.POST['title']
-    description = request.POST['description']
-    video = request.FILES["video"]
-    fs = FileSystemStorage()
-    filename = fs.save(video.name, video)
-    uploaded_file_url = fs.url(filename)
-    print(uploaded_file_url)
-    entry = AddDetails(title=title, description=description, video=filename)
-    entry.save()
-    print("entry created")
-    return render(request, 'pitcher/dashboard.html')
-
-
+def new_pitch(request):
+    print(request.POST)
+    if(len(request.POST)!=0):
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        video = request.FILES.get('video')
+        if(video!=None):
+            fs = FileSystemStorage()
+            filename = fs.save(video.name, video)
+            uploaded_file_url = fs.url(filename)
+            print(uploaded_file_url)
+            entry = AddDetails(title=title, description=description, video=filename)
+        else:
+            entry = AddDetails(title=title, description=description, video=None)
+        entry.save()
+        return render(request, 'pitcher/dashboard.html')
+    else:
+        return render(request, 'pitcher/new pitch.html')

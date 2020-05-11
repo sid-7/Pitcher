e8b7586cc705df90dd32a1090b2b40f35020e7de
Siddharth Sheladiya <siddharthsheladiya@gmail.com>
2020-05-11 16:15:42 -0700

    Update settings.py

--- application/pitcher_app/settings.py
+++ application/pitcher_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qvwm56-*o9f$2wpe*78x7g$@gj=7-!z=*@=_g#g9-ll#06ho^^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

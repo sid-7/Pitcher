1a600ed18e7a64c52a48eace4947b0b5a6b0a7ab
Siddharth Sheladiya <siddharthsheladiya@gmail.com>
2020-05-12 19:02:30 -0700

    Update views.py

--- pitcher/views.py
+++ pitcher/views.py
@@ -5,11 +5,7 @@ import datetime
 import pyrebase
 ########################  firebase authentication  ##################################
 config = {
-  "apiKey": "AIzaSyBTlSwWe6lD6NLi8OrDPe49qWIllNgttMI",
-  "authDomain": "pitcher-275100.firebaseapp.com",
-  "databaseURL": "https://pitcher-275100.firebaseio.com",
-  "storageBucket": "pitcher-275100.appspot.com",
-  "serviceAccount": "serviceAccountCredentials.json"
+  
 }
 firebase = pyrebase.initialize_app(config)
 firebase_storage = firebase.storage()
